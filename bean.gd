25f41f7aad3b21ec86fd0d2f2622a4454c2cebb4
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-11-10 19:53:28 -0500

    Proper self-righting math

--- bean.gd
+++ bean.gd
@@ -7,6 +7,14 @@ func _process(delta: float) -> void:
 	timer -= delta
 	
 	if (timer < 0):
+		
+		var rotation_axis = basis.y.cross(Vector3(0, 1, 0)).normalized()
+	
+		var rotation_angle = acos(basis.y.dot(Vector3(0, 1, 0)))
+		
+		var righting_rot = Quaternion(rotation_axis, rotation_angle).get_euler(EULER_ORDER_XYZ)
+		
+		apply_torque_impulse(righting_rot)
 		apply_central_impulse(Vector3(0, 7, 0))
-		apply_torque_impulse(Vector3(-rotation.x, 0, -rotation.z))
+		
 		timer = 5.0
