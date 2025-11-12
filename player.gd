e7218b46f7fbb51b1e866db0a812482b897b7c69
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-11-11 20:23:54 -0500

    Flinging critters

--- player.gd
+++ player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 
-var grab_swing_amount := 0.0
+var grab_x_fling := 0.0
+var grab_y_fling := 0.0
 var grabbed_critter : Node3D
 
 var camera_pitch := 0.0
@@ -22,18 +23,23 @@ func _process(delta: float) -> void:
 		position = Vector3(0, 10, 0)
 		velocity = Vector3.ZERO
 		
-	# picking up critters
+	# grabbies! uppies!
 	if (Input.is_action_just_pressed("fire")):
 		
 		if grabbed_critter:
 			
+			# letting go critters
 			grabbed_critter.reparent(get_tree().current_scene)
 			grabbed_critter.set_active(true)
+			
+			# fling!
+			grabbed_critter.linear_velocity = 40 * (Vector3(0, grab_y_fling, 0) + basis.x * grab_x_fling)
 			
 			grabbed_critter = null
 		
 		else:
-		
+			
+			# picking up critters
 			var ray_result = get_world_3d().direct_space_state.intersect_ray(
 				PhysicsRayQueryParameters3D.create(
 					$Camera3D.global_position,
@@ -51,8 +57,10 @@ func _process(delta: float) -> void:
 				grabbed_critter.rotation = Vector3.ZERO
 	
 	if grabbed_critter:
-		grabbed_critter.rotation.z = lerp(grabbed_critter.rotation.z, grab_swing_amount, delta * 10)
-	grab_swing_amount = lerp(grab_swing_amount, 0.0, delta * 10)
+		grabbed_critter.rotation.z = lerp(grabbed_critter.rotation.z, grab_x_fling * 4, delta * 10)
+		grabbed_critter.rotation.x = lerp(grabbed_critter.rotation.x, grab_y_fling * -4, delta * 10)
+	grab_x_fling = lerp(grab_x_fling, 0.0, delta * 10)
+	grab_y_fling = lerp(grab_y_fling, 0.0, delta * 10)
 	
 	# movement
 	var input_dir := Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
@@ -88,4 +96,5 @@ func _input(event):
 		camera_pitch = clampf(camera_pitch - event.relative.y * mouse_sensitivity, -90, 90)
 		
 		$Camera3D.rotation.x = deg_to_rad(camera_pitch)
-		grab_swing_amount = deg_to_rad(-event.relative.x * mouse_sensitivity * -4)
+		grab_x_fling = deg_to_rad(event.relative.x * mouse_sensitivity)
+		grab_y_fling = deg_to_rad(-event.relative.y * mouse_sensitivity)
