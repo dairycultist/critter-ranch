682c8e40f9158caa18f2426d6cbdbc1b5419e3dd
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-03-31 18:18:44 -0400

    Raycast for squishing

--- inflate/inflate_processor.gd
+++ inflate/inflate_processor.gd
@@ -3,6 +3,8 @@ extends MeshInstance3D
 @export var w: int = 4
 @export var h: int = 4
 @export var mesh_radius: float = 1.0
+@export var frames_per_mesh_update: int = 4
+var i := 0
 
 var _displace: Image
 var _displace_texture: ImageTexture
@@ -26,23 +28,31 @@ func _ready() -> void:
 
 	get_surface_override_material(0).set("shader_parameter/displace", _displace_texture);
 
-var i = 0
-
 func _process(_delta: float) -> void:
 	
-	if i % 30 != 29:
-		i += 1
+	if i != 0:
+		i -= 1
 		return
 	else:
-		i = 0
+		i = frames_per_mesh_update
 	
 	# update texture (TODO based on raycast nodes, one generated per pixel on ready)
 	_displace.fill(Color.WHITE)
 	
-	var u := randi_range(0, w - 1)
-	var v := randi_range(0, h - 1)
+	for u in range(w):
+		for v in range(h):
 	
-	_displace.set_pixel(u, v, Color.DARK_SLATE_GRAY)
+			var result = get_world_3d().direct_space_state.intersect_ray(
+				PhysicsRayQueryParameters3D.create(
+					global_position,
+					global_position + uv_to_vec3((u + 0.5) / w, (v + 0.5) / h)
+				)
+			)
+			
+			if result:
+				_displace.set_pixel(u, v, Color(1.0 - global_position.distance_to(result.position), 0.0, 0.0, 0.0))
+			else:
+				_displace.set_pixel(u, v, Color(1.0, 0.0, 0.0, 0.0))
 	
 	# update material
 	_displace_texture.update(_displace)
